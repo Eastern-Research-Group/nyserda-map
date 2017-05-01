ddf37ac2018b8f95cb3da44d9bda7b08185f2730
Bryan Neva <bryan.neva@erg.com>
2017-05-01 15:58:08 -0400

    Add functionality that copies KML out to the input parent directory

--- nyserda/__init__.py
+++ nyserda/__init__.py
@@ -1,73 +1,37 @@
 import os
-from shutil import copyfile
-import json
-
+from distutils.dir_util import copy_tree
 from .app import App
 from .kmz import Kmz
 from .kml import Kml
 from .utils import create_dir
-
+from .json_output import JsonOutput
 from nyserda.db import Session
-# from nyserda.db import User
 from nyserda.db import MapFeature
 
 def run():
     app = App()
     app.setup()
-
     kmz = Kmz(app)
 
-    # ed_user = User(name='ed',fullname='Ed Jones', password='edspassword')
-    # app.session.add(ed_user)
-    # our_user = app.session.query(User).filter_by(name='ed').first()
+    try:
+        kmls = kmz.extract_all(app.ROOT_PATH + '/input')
+        data = []
+        for foldername in kmls:
+            print(foldername)
+            kml = Kml(app,foldername, foldername)
+            data.append((foldername, kml.extract()))
+            output_base = create_dir(os.path.join(app.OUTPUT_PATH,foldername))
+            feature_collection = JsonOutput(app, foldername, output_base)
+            feature_collection.write()
 
-    # feature = MapFeature(path='foo/bar/baz', image='foo/bar/baz/0.png', coords='(1,2,3,4)')
-    # app.session.add(feature)
-    # our_feature = app.session.query(MapFeature).filter_by(path='foo/bar/baz').first()
-    # print(our_feature.image)
+            folder_path = os.path.join(app.TEMP_PATH,foldername)
+            folder_dest = os.path.join(app.INPUT_PATH,foldername)
+            copy_tree(folder_path,folder_dest)
 
-    kmls = kmz.extract_all(app.ROOT_PATH + '/input')
 
-    data = []
-    for foldername in kmls:
-        print(foldername)
-        kml = Kml(app,foldername, foldername)
-        data.append((foldername, kml.extract()))
-        output_base = create_dir(os.path.join(app.OUTPUT_PATH,foldername))
-
-        features = []
-        for instance in app.session.query(MapFeature).filter_by(key=foldername).order_by(MapFeature.id):
-            # copy image to output with unique name
-            filename = "%s_%s.png" % (instance.id, instance.key)
-            output_path = os.path.join(output_base,filename)
-            try:
-                copyfile(instance.path, output_path)
-            except OSError:
-                pass
-
-            # Write coordinates into json with new path name (s,w,n,e)
-            coords = instance.coords.split(',')
-            features.append({
-                'type': 'Feature',
-                'properties': {
-                    'name': '%s_%s' % (instance.id, instance.key),
-                    'image_overlay': filename
-                },
-                'geometery': {
-                    'type': 'Point',
-                    'coordinates': [[coords[0],coords[1]],[coords[2],coords[3]]],
-                }
-            })
-
-        # Create json / GeoJSON file
-        feature_collection = {'type': 'FeatureCollection', 'features': features}
-
-        try:
-            output_file = os.path.join(output_base,foldername + '.json')
-            f = open(output_file, 'w+')
-            f.write(json.dumps(feature_collection))
-        except OSError:
-            pass
+    except OSError as e:
+        # print(e.errno)
+        pass
 
     app.cleanup()
 
---
+++ nyserda/json_output.py
@@ -0,0 +1,50 @@
+import os
+import json
+from shutil import copyfile
+from nyserda.db import MapFeature
+
+class JsonOutput:
+    def __init__(self, app, key, output_base):
+        self.features = []
+        self.output_base = output_base
+        self.key = key
+        self.app = app
+        self.generate()
+
+    def generate(self):
+        for instance in self.app.session.query(MapFeature).filter_by(key=self.key).order_by(MapFeature.id):
+            # copy image to output with unique name
+            print(instance.path)
+            filename = "%s_%s.png" % (instance.id, instance.key)
+            output_path = os.path.join(self.output_base,filename)
+            try:
+                copyfile(instance.path, output_path)
+            except OSError:
+                pass
+
+            # Write coordinates into json with new path name (s,w,n,e)
+            coords = instance.coords.split(',')
+            self.features.append({
+                'type': 'Feature',
+                'properties': {
+                    'name': '%s_%s' % (instance.id, instance.key),
+                    'image_overlay': filename
+                },
+                'geometery': {
+                    'type': 'Point',
+                    'coordinates': [[coords[0],coords[1]],[coords[2],coords[3]]],
+                }
+            })
+
+    # Create json / GeoJSON file
+    def toJSON(self):
+        return json.dumps({'type': 'FeatureCollection','features': self.features})
+
+
+    def write(self):
+        try:
+            output_file = os.path.join(self.output_base,self.key + '.json')
+            f = open(output_file, 'w+')
+            f.write(self.toJSON())
+        except OSError:
+            pass
